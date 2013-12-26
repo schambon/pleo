075e8f1b76d78e4b08cb789a83dd6bccea0736d6
Sylvain Chambon <sylvain.chambon@gmail.com>
2013-12-26 13:06:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pleo2::Application.config.secret_key_base = '81eff904b71695c9372dcfb91278d1ec66f8a6885dff1a8fcaf5c7e784ee5df0b0e4ddd15e66476172b6abc409b28ecefa15c0937f066ff339fd48fe71e93d5b'
