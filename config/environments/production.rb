bca7df8e53455b94704383df545b419b29fda2cd
Sylvain Chambon <sylvain.chambon@gmail.com>
2014-01-05 21:23:09 +0100

    Do no log text

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Pleo2::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.filter_parameters += :text
+  config.filter_parameters += [:text]
 end
